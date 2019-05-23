827deba0a0ebd920bc50152e984ca02d375a08ef
Fernando Lima <fernandovlima.s@gmail.com>
2019-05-23 11:22:11 -0300

    create action for authed user

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,16 @@
 import { getInitalData } from "../utils/api";
-//actions
+//actions creaters
 import { receiveUsers } from "../actions/users";
 import { receiveTweets } from "../actions/tweets";
+import { setAuthedUser } from "../actions/authedUser";
 
+const AUTHED_ID = "dan_abramov";
 export function handleInitialData() {
   return dispatch => {
     return getInitalData().then(({ users, tweets }) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUsers(users));
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
